4d941f766b8ed371488c18f44265f8aed0c25139
David Cantú <32000336+codyvi@users.noreply.github.com>
2019-02-28 15:08:10 -0600

    Add files via upload

---
+++ Algoritmos/Floyd/Floyd.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <climits>
+
+using namespace std;
+
+void floyd(int D[20][20], int n)
+{
+	for(int k = 0; k < n; k++)
+    {
+		for(int i = 0; i < n; i++)
+        {
+			for(int j = 0; j < n; j++)
+            {
+				if(D[i][k] != INT_MAX && D[k][j] != INT_MAX && D[i][k]+D[k][j] <D[i][j])
+                {
+					D[i][j] = D[i][k] + D[k][j];
+				}
+			}
+		}
+	}
+}
+
+int main()
+{
+	int n, m, q, a, b, c;
+	int D[20][20];
+	cin >> n >> m >> q;
+	for(int i = 0; i < n ; i++ )
+    {
+		D[i][i] = 0;
+		for(int j = i+1; j < n; j++)
+        {
+			D[i][j] = D[j][i] = INT_MAX;
+		}
+	}
+
+	for(int i = 0; i < m ; i++)
+    {
+		cin >> a >> b >> c;
+		D[a-1][b-1] = D[b-1][a-1] = c;
+	}
+	
+    floyd(D,n);
+
+	for(int i = 0; i < q ; i++)
+    {
+		cin >> a >> b;
+		cout << D[a-1][b-1] << endl;
+	}
+
+}
